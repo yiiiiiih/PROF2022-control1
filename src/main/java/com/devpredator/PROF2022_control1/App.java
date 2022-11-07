62b3c0f2fa47428e9b7dd00e0fd9a8f5d385439e
Yiyuan Huang <yiyuan.huang@alumnos.upm.es>
2022-11-07 18:06:23 +0100

    first commit

---
+++ src/main/java/com/devpredator/PROF2022_control1/App.java
@@ -0,0 +1,13 @@
+package com.devpredator.PROF2022_control1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
